9dbd04585ee059074bbbbb10cf93f375ebd95832
Hack Reactor Students <github@hackreactor.com>
2013-08-15 11:28:42 -0700

    Finished unoptimized version of rook solution

--- src/solvers.js
+++ src/solvers.js
@@ -3,16 +3,37 @@
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 
 window.findNRooksSolution = function(n){
-  var solution = undefined; //fixme
-  debugger;
-  var board = new Board([
-      [0, 1, 0, 0],
-      [0, 0, 1, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0]]);
-  console.log(board.hasAnyMajorDiagonalConflicts());
+  var solution;
+  // Start with an empty board
+  var boardLayout = [];
+  var newRow = [];
+  for (var i = 0; i < n; i++) {
+    newRow.push(0);
+  }
+  for (var j = 0; j < n; j++) {
+    boardLayout[j] = newRow.slice();
+  }
+  var board = new Board(boardLayout);
+  var traverser = function(boardLayout, newRowNumber) {
+    // Unoptimized traverser
+    var traverseBoard = new Board(boardLayout);
+    // thisRow = array of 0s
+    var thisRow = traverseBoard.get(newRowNumber);
+    for (var i = 0; i < thisRow.length; i++) {
+      thisRow[i] = 1;
+      traverseBoard.set(newRowNumber,thisRow.slice());
+      thisRow[i] = 0;
+      if (newRowNumber === thisRow.length-1) {
+        if (!traverseBoard.hasAnyRooksConflicts()) {
+          board = new Board(traverseBoard.rows());
+        }
+      } else traverser(traverseBoard.rows(),newRowNumber+1);
+    }
+  };
+  traverser(board.rows(),0);
   console.log('Single solution for ' + n + ' rooks:', solution);
-  return solution;
+  console.log(board.rows());
+  return board.rows();
 };
 
 window.countNRooksSolutions = function(n){
